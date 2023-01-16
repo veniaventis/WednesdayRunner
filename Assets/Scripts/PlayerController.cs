a7c2ed58d2de67a87ff2528b72f644c2be01f9dc
Daniel <4aba3691@gmail.com>
2023-01-16 22:02:52 +0100

    Added score and Lose panel nned to fix collider in axe and saw

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int speed;
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravity;
+    [SerializeField] private GameObject losePanel;
 
     private int lineToMove = 1;
     public float lineDistance = 4;
@@ -45,8 +46,15 @@ public class PlayerController : MonoBehaviour
         else if(lineToMove == 2)
             targetPosition += Vector3.right *lineDistance;
 
-        transform.position = targetPosition;
-    }
+        if(transform.position == targetPosition)
+            return;
+        Vector3 diff = targetPosition - transform.position;
+        Vector3 moveDir = diff.normalized * 25 *Time.deltaTime;
+        if(moveDir.sqrMagnitude < diff.sqrMagnitude)
+            controler.Move(moveDir);
+        else
+            controler.Move(diff);
+            }
 
     private void Jump()
     {
@@ -59,5 +67,14 @@ public class PlayerController : MonoBehaviour
         dir.z = speed;
         dir.y += gravity * Time.fixedDeltaTime;
         controler.Move(dir * Time.fixedDeltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "obstacle")
+        {
+            losePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
